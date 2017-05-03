32369e71d2f08eec8cffe92ee262e646cfb9583c
Daniel Bednorz <daniel.bednorz@novatec-gmbh.de>
2017-05-03 10:12:52 +0200

    fixed bean-injection for RestTemplate; added codecov;

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 [![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)
 [![Build Status](https://travis-ci.org/nt-ca-aqe/marketing-metrics-collector.svg?branch=master)](https://travis-ci.org/nt-ca-aqe/marketing-metrics-collector)
+[![codecov](https://codecov.io/gh/nt-ca-aqe/marketing-metrics-collector/branch/master/graph/badge.svg)](https://codecov.io/gh/nt-ca-aqe/marketing-metrics-collector)
--- mmc-commons/src/main/java/info/novatec/metricscollector/commons/CommonsApplicationInitializer.java
+++ mmc-commons/src/main/java/info/novatec/metricscollector/commons/CommonsApplicationInitializer.java
@@ -3,26 +3,17 @@ package info.novatec.metricscollector.commons;
 import org.influxdb.InfluxDBFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+
+import lombok.Setter;
 
 
 @SpringBootApplication
 @ConfigurationProperties(prefix = "influx")
 public class CommonsApplicationInitializer {
 
-    @lombok.Setter
+    @Setter
     String url;
-
-//    public static void main(String[] args) {
-//        SpringApplication.run(ApplicationInitializerCommons.class, args);
-//    }
-
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplateBuilder().build();
-    }
 
     @Bean
     public InfluxService influxService() {
